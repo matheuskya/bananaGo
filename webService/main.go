9aceed56033fc833f7a4313dc2f8c81c90020822
Matheus Kenzo <100180936+matheuskya@users.noreply.github.com>
2025-02-03 17:35:49 -0300

    updated main.go

--- webService/main.go
+++ webService/main.go
@@ -1,9 +1,17 @@
 package main
 
-import (
-	"fmt"
-)
+type fruit struct {
+	Id     int  `json:"id"`
+	Name   string `json:"name"`
+	Flavor string `json:"flavor"`
+	Color  string `json:"color"`
+}
 
-func main() {
-	fmt.Println("hello world")
+var fruits []fruit = {
+{
+    Id: 0,
+    Name: "banana",
+    Flavour
+
+},
 }
