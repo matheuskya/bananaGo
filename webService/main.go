c0618b2ca7379e72728c15ad27132f4e95f14d96
Matheus Kenzo <100180936+matheuskya@users.noreply.github.com>
2025-02-06 17:21:24 -0300

    updated main.go

--- webService/main.go
+++ webService/main.go
@@ -1,17 +1,64 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type fruit struct {
-	Id     int  `json:"id"`
+	Id     string `json:"id"`
 	Name   string `json:"name"`
 	Flavor string `json:"flavor"`
 	Color  string `json:"color"`
 }
 
-var fruits []fruit = {
-{
-    Id: 0,
-    Name: "banana",
-    Flavour
+var fruits []fruit = []fruit{
+	{
+		Id:     0,
+		Name:   "banana",
+		Flavor: "sweet",
+		Color:  "yelllow",
+	},
+	{
+		Id:     1,
+		Name:   "lemon",
+		Flavor: "sour",
+		Color:  "yellow",
+	},
+}
 
-},
+func getFruits(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, fruits)
+}
+
+func postAlbums(c *gin.Context) {
+	var newFruit fruit
+
+	if err := c.BindJSON(&newFruit); err != nil {
+		return
+	}
+
+	fruits = append(fruits, newFruit)
+	c.IndentedJSON(http.StatusCreated, newFruit)
+}
+
+func getFruitById(c *gin.Context) {
+	id := c.Param("id")
+	for _, a := range fruits {
+		if a.Id == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Fruit not found :("})
+
+}
+
+func main() {
+	fmt.Println("Hello world")
+	router := gin.Default()
+	router.GET("/fruits", getFruits)
+	router.Run("localhost:8080")
 }
